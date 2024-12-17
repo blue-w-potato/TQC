04ae5d868f7119281ae2d9b37affda662f87c1fe
blue-w-potato <wame20080418@gmail.com>
2024-12-17 22:53:51 +0800

    ˙˙

--- 秒殺TQC/602.py
+++
@@ -1,9 +0,0 @@
-# A = 1
-# J = 11
-# Q = 12
-# K = 13
-
-a = ' '.join([ input() for i in range(5) ]) # 把五張牌的點數用空格隔開
-for i in ('A1', 'J11', 'Q12', 'K13'):
-    a = a.replace(i[0], i[1:]) # i[0] 是字母 ， 字母後面全部的字是他的點數
-print(sum(map(int, a.split()))) # a 用空格隔開所以這裡用空個切開，全部轉整數之後再加總
--- 秒殺TQC/604.py
+++
@@ -1,7 +0,0 @@
-a = [ int(input()) for i in range(10) ] # 10 個整數
-n = [0,0] # 用來存出現次數最多的那個數 [ 那個數, 出現次數 ]
-for i in set(a): # 去重複
-    s = a.count(i) # 出現次數
-    if s > n[1]: # 如果 出現次數 比目前最多的還多
-        n = [i, s] # 更新紀錄
-for i in n:print(i) # 分兩行輸出 n
