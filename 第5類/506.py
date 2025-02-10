129958a960a5a0fa0ace4ef81ed5f1ce4cd687a8
blue-w-potato <wame20080418@gmail.com>
2025-02-10 22:48:39 +0800

    蛤阿

--- 第5類/506.py
+++ 第5類/506.py
@@ -1,14 +1,17 @@
-def compute(a, b, c):
-    d = b**2 - 4*a*c
-    if d < 0:
-        return []
+a,b,c = int(input()),int(input()),int(input())
+def compute(a,b,c):
+    d = b**2-4*a*c
+    if d<0:
+        return False
     if d == 0:
-        return [-b/2/a]
-    return sorted([(-b-d**0.5)/2/a, (-b+d**0.5)/2/a], reverse=True)
-a,b,c = int(input()), int(input()), int(input())
-k = compute(a,b,c)
-if k:
-    print(k[0], end='')
-    if len(k)>1: print(',',k[1])
+        return [(-b)/(2*a)]
+    return [(-b+d**0.5)/(2*a), (-b-d**0.5)/(2*a)]
+a = compute(a,b,c)
+if a:
+    if len(a) == 1:
+        print(a[0])
+    else:
+        a,b = a
+        print( f'{a}, {b}' )
 else:
     print("Your equation has no root.")
